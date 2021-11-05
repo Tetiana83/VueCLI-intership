fad16a034694ec18b734d79aafb46c4340ce8970
Tetiana83 <itena@ukr.net>
2021-11-05 16:54:24 +0200

    Lesson-4 Add interface

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -9,8 +9,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import TaskComponent from '../components/TaskComponent.vue'
-// import Itask from '../types/tasks.interface'
-
+import Itask from '../types/tasks.interface'
+const tasks: Itask[] = [
+  { id: 1, title: 'Lesson 1', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', datEnd: '31/10/2021', color: '#cf9d9d' },
+  { id: 2, title: 'Lesson 2', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', datEnd: '01/11/2021', color: '#a52da7' },
+  { id: 3, title: 'Lesson 3', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', datEnd: '03/11/2021', color: '#232daf' },
+  { id: 4, title: 'Lesson 4', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', datEnd: '05/11/2021', color: '#23af68' },
+  { id: 5, title: 'Homework', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', datEnd: '10/11/2021', color: '#af8a23' }
+]
 export default defineComponent({
   name: 'Task',
   components: {
@@ -18,13 +24,7 @@ export default defineComponent({
   },
   data () {
     return {
-      taskList: [
-        { id: 1, title: 'Lesson 1', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', datEnd: '31/10/2021', color: '#cf9d9d' },
-        { id: 2, title: 'Lesson 2', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', datEnd: '01/11/2021', color: '#a52da7' },
-        { id: 3, title: 'Lesson 3', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', datEnd: '03/11/2021', color: '#232daf' },
-        { id: 4, title: 'Lesson 4', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', datEnd: '05/11/2021', color: '#23af68' },
-        { id: 5, title: 'Homework', desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.', datEnd: '10/11/2021', color: '#af8a23' }
-      ]
+      taskList: tasks
     }
   },
   methods: {
